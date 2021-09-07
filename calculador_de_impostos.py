3d06bcf0f243d66eb985c065565a6997fd711d1c
João Pedro <sousajoaopedro1@gmail.com>
2021-09-07 18:44:56 -0300

    terceiro capitulo finalizado

--- calculador_de_impostos.py
+++ calculador_de_impostos.py
@@ -1,18 +1,29 @@
 from typing import Union
 
-from impostos import ICMS, ISS
-from orcamento import Orcamento
+from impostos import ICMS, ISS, ICPP, IKCV
+from orcamento import Orcamento, Item
+
+impostos = Union[ICMS, ISS, ICPP, IKCV]
 
 
 class CalculadorImpostos:
     @staticmethod
-    def realiza_calculo(orcamento: Orcamento, imposto: Union[ICMS, ISS]) -> None:
+    def realiza_calculo(orcamento: Orcamento, imposto: impostos) -> None:
         imposto_calculado: float = imposto.calcula(orcamento)
         print(imposto_calculado)
 
 if __name__ == '__main__':
     calculador = CalculadorImpostos()
-    orcamento_1 = Orcamento()
 
+    orcamento_1 = Orcamento()
+    orcamento_1.adiciona_item(Item("ITEM 1", 50))
+    orcamento_1.adiciona_item(Item("ITEM 2", 200))
+    orcamento_1.adiciona_item(Item("ITEM 3", 250))
+
+    print("ISS e ICMS")
     calculador.realiza_calculo(orcamento_1, ISS())
     calculador.realiza_calculo(orcamento_1, ICMS())
+
+    print("ICPP e IKCV")
+    calculador.realiza_calculo(orcamento_1, ICPP())
+    calculador.realiza_calculo(orcamento_1, IKCV())
--- impostos.py
+++ impostos.py
@@ -1,15 +1,73 @@
+from abc import ABCMeta, abstractmethod
+
+from orcamento import Orcamento
+
+
+class TemplateImpostoCondicional(metaclass=ABCMeta):
+    def calcula(self, orcamento):
+        if self.deve_usar_maxima_taxacao(orcamento):
+            return self.maxima_taxacao(orcamento)
+        else:
+            return self.minima_taxacao(orcamento)
+
+    @abstractmethod
+    def deve_usar_maxima_taxacao(self, orcamento):
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def maxima_taxacao(orcamento):
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def minima_taxacao(orcamento):
+        ...
+
 
 class ISS:
     @staticmethod
-    def calcula(orcamento) -> float:
+    def calcula(orcamento: Orcamento) -> float:
         return orcamento.valor * 0.1
 
 
 class ICMS:
     @staticmethod
-    def calcula(orcamento) -> float:
+    def calcula(orcamento: Orcamento) -> float:
         return orcamento.valor * 0.06
 
+
+class ICPP(TemplateImpostoCondicional):
+    def deve_usar_maxima_taxacao(self, orcamento) -> bool:
+        return orcamento.valor > 500
+
+    @staticmethod
+    def maxima_taxacao(orcamento) -> float:
+        return orcamento.valor * 0.07
+
+    @staticmethod
+    def minima_taxacao(orcamento) -> float:
+        return orcamento.valor * 0.05
+
+
+class IKCV(TemplateImpostoCondicional):
+    def deve_usar_maxima_taxacao(self, orcamento) -> bool:
+        return orcamento.valor > 500 and self.__tem_item_maior_que_100_reais(orcamento)
+
+    @staticmethod
+    def maxima_taxacao(orcamento) -> float:
+        return orcamento.valr * 0.1
+
+    @staticmethod
+    def minima_taxacao(orcamento) -> float:
+        return orcamento.valor * 0.06
+
+    @staticmethod
+    def __tem_item_maior_que_100_reais(orcamento: Orcamento) -> bool:
+        for item in orcamento.obter_itens():
+            if item.valor > 100:
+                return True
+        return False
 
 if __name__ == '__main__':
     pass
